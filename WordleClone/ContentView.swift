8bfbea3394b59c4d406640229c12fefea3990983
Tino Tusaumwe <tusatino@gmail.com>
2023-01-28 23:28:25 +1000

    Add GameView to ContentView

--- WordleClone/ContentView.swift
+++ WordleClone/ContentView.swift
@@ -6,23 +6,10 @@
 //
 
 import SwiftUI
-let words = [
-    "quiet",
-    "acorn",
-    "chuck",
-    "trick",
-    "allow",
-]
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        GameView()
     }
 }
 
